3c4e518deb15f257667af62b74119197c16cb73e
Arpit kamal <arpit@Arpits-MacBook-Pro.local>
2021-03-20 22:16:53 +0530

    survey views, routes and partials added

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+/*--COMP229-w2021-TeamProject server/config/db.js; Dominators; 19Mar2021 */
+
+//define database URI
+module.exports =
+{
+    //URI to MongoAtlas
+    "URI": "mongodb+srv://admin:nuiA68PUz74AsvoU@surveysite.jbtw8.mongodb.net/SurveyList?retryWrites=true&w=majority"
+}
---
+++ server/controllers/index.js
@@ -0,0 +1,27 @@
+/*--COMP229-w2021-TeamProject server/controllers/index.js; Dominators; 19Mar2021 */
+
+//require modules for the controller
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//create a reference to the model
+let Survey = require('../models/surveys');
+
+module.exports.displayHomePage = (req, res, next) => {
+    Survey.find((err, surveyList) => {
+        if(err)
+        {
+                return console.error(err);
+        }
+        else
+        {
+            //console.log(ContactList);
+
+            res.render('index', 
+            {title: 'Home', 
+            SurveyList: surveyList});
+        }
+    }).sort({name: 1});
+}
+
